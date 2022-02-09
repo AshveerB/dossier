fc9e696e00e83075e260fe146dd404a71beea111
AshveerB <ashveerbhayroo@gmail.com>
2022-02-09 10:49:49 -0800

    update about description

--- src/components/data.js
+++ src/components/data.js
@@ -5,7 +5,7 @@ export const title = 'Software Engineer';
 export const promises = ['Fast\n', 'Responsive\n', 'Dynamic\n', 'Accessible\n'];
 
 export const description =
-	'I am a full stack engineer; I create responsive applications, solve complex problems and streamline work for clients through the software I develop. I am passionate about helping people and creating the most innovative tools for my clients in order to simplify their daily lives. I am curious, hard-working and adaptive which enables me to succeed under any condition.';
+	'I am a full stack engineer; I create responsive applications, solve complex problems and streamline work for clients through the software I develop. I am passionate about helping people and creating the most innovative tools for my clients in order to simplify their daily lives. I am curious, hard-working and adaptive which enables me to succeed under any condition. Martial arts student and SF 49er fanatic!';
 
 export const contactLinks = [
 	{
